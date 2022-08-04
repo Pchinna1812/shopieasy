6d284a6d86e4832063f1adc6d0314e79d7681d81
pchinna1812 <pchinna1812@gmail.com>
2022-08-04 16:14:46 +0530

    first commit

---
+++ src/main/java/com/database/shopieasyApplication.java
@@ -0,0 +1,13 @@
+package com.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class shopieasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(shopieasyApplication.class, args);
+	}
+
+}
